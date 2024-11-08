683c67718dd15ed724b17b9e652ca03f313ea186
lai55091017 <lai55091017@gmail.com>
2024-11-08 14:27:27 +0800

    又改一點

--- web/src/js/PopupWindow.js
+++ web/src/js/PopupWindow.js
@@ -3,13 +3,14 @@
 import * as THREE from 'three';
 
 class PopupWindow {
-    constructor(){
+    constructor() {
         this.popup = document.createElement('div');
-        this.popup.style.position = 'absolute'; // 設定彈窗為絕對位置(absolute)，如果有問題再試試看fixed
-        this.popup.style.background = 'rgba(0, 0, 0, 0.8)'; // 背景為透明度80%的黑底
-        this.popup.style.color = 'white'; // 顏色為白色
-        this.popup.style.padding = '10px'; // 內留白為10px
-        this.popup.style.borderRadius = '5px'; // 圓角5px，不是必需的，只是為了讓它看起來沒那麼尖銳
+        this.popup.className = 'model_info';
+        // this.popup.style.position = 'absolute'; // 設定彈窗為絕對位置(absolute)，如果有問題再試試看fixed
+        // this.popup.style.background = 'rgba(0, 0, 0, 0.8)'; // 背景為透明度80%的黑底
+        // this.popup.style.color = 'white'; // 顏色為白色
+        // this.popup.style.padding = '10px'; // 內留白為10px
+        // this.popup.style.borderRadius = '5px'; // 圓角5px，不是必需的，只是為了讓它看起來沒那麼尖銳
         this.popup.style.display = 'none'; // 初始為隱藏(因為是互動後才彈出所以要先隱藏)
         document.body.appendChild(this.popup);
     }
@@ -19,8 +20,8 @@ class PopupWindow {
             <p>中文名稱：${chineseName}</p>
             <p>英文名稱：${englishName}</p>
         `; // 這裡使用的是``而不是''，``可以創建多行字串、使用${}直接插入變數或表達式。而''只能創建單行字串且需使用+來串接變數
-        this.popup.style.left = `${position.x}px`;
-        this.popup.style.top = `${position.y}px`;
+        // this.popup.style.left = `${position.x}px`;
+        // this.popup.style.top = `${position.y}px`;
         this.popup.style.display = 'block'; // 顯示彈窗
     }
 
